169965975b0b039a92829403e26e4a2458650da4
athdromeda <anixatho01@gmail.com>
2023-01-21 22:36:42 +0700

    feature: [Time] Add time input
    Install momentJs and add time input

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,11 @@ import { Canvas } from "@react-three/fiber";
 import { Earth } from "./Earth";
 import { Moon } from "./Moon";
 import "./App.css";
-import { OrthographicCamera, OrbitControls } from "@react-three/drei";
+import { OrthographicCamera, OrbitControls, Stats } from "@react-three/drei";
 import { julian, moonposition, solar } from "astronomia";
 import { Sun } from "./Sun";
 import { useEffect } from "react";
-
-const jde = julian.CalendarGregorianToJD(2023, 1, 20);
-const moonPos = moonposition.position(jde);
-const solarPos = solar.trueEquatorial(jde);
-const deltaRA = Math.abs(solarPos._ra - moonPos._ra);
-const D2R = Math.PI / 180;
+import moment from "moment";
 
 const EqPlane = (props) => {
   const mesh = useRef();
@@ -25,11 +20,26 @@ const EqPlane = (props) => {
 };
 
 function App() {
-  const [current, setCurrent] = useState(new Date());
+  const [date, setDate] = useState(moment());
+  const [time, setTime] = useState(moment());
+  const [finalDate, setFinalDate] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => setCurrent(new Date()), 1000);
-  }, []);
+    setFinalDate(
+      new Date(`${date.format("YYYY-MM-DD")}T${time.format("HH:mm")}:00`)
+    );
+  }, [date, time]);
+
+  const jde = julian.DateToJD(finalDate);
+  const moonPos = moonposition.position(jde);
+  const solarPos = solar.trueEquatorial(jde);
+  const deltaRA = Math.abs(solarPos._ra - moonPos._ra);
+  const D2R = Math.PI / 180;
+
+  // useEffect(() => {
+  //   setInterval(() => setdate(localdate), 10000);
+  // }, []);
+
   return (
     <>
       <Canvas camera={{ position: [-600, 100, 400] }}>
@@ -40,7 +50,7 @@ function App() {
           position={[0, 0, 0]}
           rotation={[
             0,
-            0 + Math.PI / 2 + current.getUTCHours() * 15 * D2R, //rotate earth to greenwich
+            0 + Math.PI / 2 + date._d.getUTCHours() * 15 * D2R, //rotate earth to greenwich
             0,
           ]}
         />
@@ -64,11 +74,27 @@ function App() {
 
       <section>
         <p>{"JDE = " + jde}</p>
-        <div>
-          <input type="date" name="" id="" />
-          <input type="time" name="" id="" />
+        <div className="timepicker">
+          <input
+            type="date"
+            value={date.format("YYYY-MM-DD")}
+            onChange={(e) => setDate(moment(e.target.value, "YYYY-MM-DD"))}
+          />
+          <input
+            type="time"
+            value={time.format("HH:mm")}
+            onChange={(e) => setTime(moment(e.target.value, "HH:mm"))}
+          />
+          <button
+            onClick={() => {
+              setTime(moment());
+              setDate(moment());
+            }}
+          >
+            reset
+          </button>
         </div>
-        <div>
+        <div className="panel">
           <a>
             Sun (RA, Dec) : ({solarPos._ra},{solarPos._dec})
           </a>
@@ -76,6 +102,7 @@ function App() {
           <a>
             Moon (RA, Dec) : ({moonPos._ra},{moonPos._dec})
           </a>
+          <a>{JSON.stringify(time)}</a>
         </div>
       </section>
     </>
