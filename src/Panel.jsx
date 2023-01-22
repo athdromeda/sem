fc45f4e89e8b8c6575189736e8f488288ab441b5
athdromeda <anixatho01@gmail.com>
2023-01-22 16:48:29 +0700

    feat: [Panel] Show sun and moon data
    Show sun and moon right ascension and declination

--- src/Panel.jsx
+++ src/Panel.jsx
@@ -1,39 +1,47 @@
 import moment from "moment";
+import { julian, moonposition, solar } from "astronomia";
 
-export default function Panel({date, time, setDate, setTime}) {
+export default function Panel({ date, time, setDate, setTime }) {
+  const D2R = Math.PI / 180;
+  const R2D = 180 / Math.PI;
+  const jde = julian.DateToJD(date._d);
+  const moonPos = moonposition.position(jde);
+  const solarPos = solar.trueEquatorial(jde);
+
   return (
     <section>
-        {/* <p>{"JDE = " + jde}</p> */}
-        <div className="timepicker">
-          <input
-            type="date"
-            value={date.format("YYYY-MM-DD")}
-            onChange={(e) => setDate(moment(e.target.value, "YYYY-MM-DD"))}
-          />
-          <input
-            type="time"
-            value={time.format("HH:mm")}
-            onChange={(e) => setTime(moment(e.target.value, "HH:mm"))}
-          />
-          <button
-            onClick={() => {
-              setTime(moment());
-              setDate(moment());
-            }}
-          >
-            reset
-          </button>
-        </div>
-        <div className="panel">
-          <a>
-            {/* Sun (RA, Dec) : ({solarPos._ra},{solarPos._dec}) */}
-          </a>
-          <br />
-          <a>
-            {/* Moon (RA, Dec) : ({moonPos._ra},{moonPos._dec}) */}
-          </a>
-          <a>{JSON.stringify(time)}</a>
-        </div>
-      </section>
-  )
+      <p>{"JDE = " + jde}</p>
+      <div className="timepicker">
+        <input
+          type="date"
+          value={date.format("YYYY-MM-DD")}
+          onChange={(e) => setDate(moment(e.target.value, "YYYY-MM-DD"))}
+        />
+        <input
+          type="time"
+          value={time.format("HH:mm")}
+          onChange={(e) => setTime(moment(e.target.value, "HH:mm"))}
+        />
+        <button
+          onClick={() => {
+            setTime(moment());
+            setDate(moment());
+          }}
+        >
+          reset
+        </button>
+      </div>
+      <div className="panel">
+        <a>
+          Sun (RA, Dec) : ({solarPos._ra*R2D},{solarPos._dec*R2D})
+        </a>
+        <br />
+        <a>
+          Moon (RA, Dec) : ({moonPos._ra*R2D},{moonPos._dec*R2D})
+        </a>
+        <br />
+        <a>{JSON.stringify(time)}</a>
+      </div>
+    </section>
+  );
 }
